e69e7d7441229ce30939ad19dab13161d716508d
MarinaYanovskiy <marinay@campus.technion.ac.il>
2025-01-11 21:42:24 +0200

    parts 3+4

--- hw2/hw2_cdp/filters.py
+++ hw2/hw2_cdp/filters.py
@@ -6,10 +6,15 @@
 # "Concurrent and Distributed Programming for Data processing
 # and Machine Learning" course (02360370), Winter 2024
 #
-from numba import cuda
+# import os
+# os.environ["NUMBA_ENABLE_CUDASIM"] = "1"
+
+import numpy as np
+from numba import cuda, prange
 from numba import njit
 import imageio
 import matplotlib.pyplot as plt
+import math
 
 def correlation_gpu(kernel, image):
     '''Correlate using gpu
@@ -19,12 +24,81 @@ def correlation_gpu(kernel, image):
         A small matrix
     image : numpy array
         A larger matrix of the image pixels
-            
+
     Return
     ------
     An numpy array of same shape as image
     '''
-    raise NotImplementedError("To be implemented")
+
+    # res- the result of correlation calculation between image and kernel.
+    img_rows, img_cols = image.shape
+    res = np.zeros((img_rows, img_cols))
+
+    #allocate  everything on gpu memory
+    device_img = cuda.to_device(image)
+    device_ker = cuda.to_device(kernel)
+    device_res = cuda.to_device(res)
+
+    threads_per_block = (16, 16)  # 16x16 threads per block
+    blocks_per_grid = (
+        math.ceil(img_rows / threads_per_block[0]),
+        math.ceil(img_cols / threads_per_block[1])
+    )
+
+    # Launch kernel. Blocks = img_rows, Threads_per_block = img_cols
+    correlation_kernel[threads_per_block, blocks_per_grid](device_img, device_ker, device_res)
+
+    # Copy the result back to the host
+    res = device_res.copy_to_host()
+
+    return res
+
+@cuda.jit
+def correlation_kernel(image, kernel, output):
+    ''' Handles one cell of the image.
+
+    Parameters
+    ----------
+    image: numpy array located on GPU.
+    kernel: numpy array located on GPU.
+    output: the output matrix, located on GPU as well.
+
+    Returns
+    -------
+    void
+
+    '''
+    rows, cols = kernel.shape
+    img_rows, img_cols = image.shape
+    x_padd = rows // 2
+    y_padd = cols // 2
+    thread_x = cuda.threadIdx.x
+    thread_y = cuda.threadIdx.y
+
+    # get the index of the cell we are working on
+    x = thread_x + cuda.blockIdx.x * cuda.blockDim.x
+    y = thread_y + cuda.blockIdx.y * cuda.blockDim.y
+
+    #check if it is valid
+    if(x >= img_rows or y >= img_cols):
+        return
+
+    #initialize a local variable to write to, instead of writing to global mem. each iteration
+    res = 0.0
+
+    #calculate the correlation
+    for row in range(rows):
+        for col in range(cols):
+            #check if the index is valid in the image.
+            # if not - we won't do any addition, which equals to multiplying by 0 and adding the result
+            idx_x = x - x_padd + row
+            idx_y = y - y_padd + col
+            if (idx_x >= 0 and idx_x < img_rows and idx_y >= 0 and idx_y < img_cols):
+               res  += image[idx_x, idx_y] * kernel[row, col]
+
+    # now, preform one write to the global memory.
+    output[x, y] = res
+
 
 
 @njit
@@ -36,12 +110,35 @@ def correlation_numba(kernel, image):
         A small matrix
     image : numpy array
         A larger matrix of the image pixels
-            
+
     Return
     ------
     An numpy array of same shape as image
     '''
-    raise NotImplementedError("To be implemented")
+    # first- padd the matrix with zeros so the correlation computation woll always be in bound.
+    rows, cols = kernel.shape
+    x_padd = rows // 2
+    y_padd = cols // 2
+
+    # result - the result of correlation calculation between image and kernel.
+    img_rows, img_cols = image.shape
+    result = np.zeros((img_rows, img_cols))
+
+    for i in prange(img_rows):
+        for j in prange(img_cols):
+
+            # a local variable to calculate into
+            res = 0.0
+            for offset_x in prange(rows):
+                for offset_y in prange(cols):
+                    indx_x = i + offset_x - x_padd
+                    indx_y = j + offset_y - y_padd
+                    if(indx_x >= 0 and indx_y >= 0 and indx_x < img_rows and indx_y < img_cols):
+                        res += kernel[offset_x, offset_y] * image[indx_x, indx_y]
+            # now add the local calculation to memory
+            result[i, j] = res
+
+    return result
 
 
 def sobel_operator():
@@ -51,13 +148,32 @@ def sobel_operator():
         ------
         An numpy array of the image
         '''
-    pic = load_image()
     # your calculations
+    pic = load_image()
+    sobel_filter_submission = np.array([[1, 0, -1],
+                                        [2, 0, -2],
+                                        [1, 0, -1]])
 
-    raise NotImplementedError("To be implemented")
+    # sobel_filter1 = np.array([[3, 0, -3],
+    #                           [10, 0, -10],
+    #                           [3, 0, -3]])
+
+    # sobel_filter2 = np.array([[1, 0, -1],
+    #                           [2, 0, -1],
+    #                           [1, 0, -2],
+    #                           [2, 0, -2],
+    #                           [1, 0, -1]])
+
+    # sobel_filter3 = np.array([[1, 1, 1],
+    #                           [1, 0, 1],
+    #                           [1, 1, 1]])
+
+    Gx = correlation_numba(sobel_filter_submission, pic)
+    Gy = correlation_numba(np.transpose(sobel_filter_submission), pic)
+    return np.sqrt((Gx ** 2) + (Gy ** 2))
 
 
-def load_image(): 
+def load_image():
     fname = 'data/image.jpg'
     pic = imageio.imread(fname)
     to_gray = lambda rgb : np.dot(rgb[... , :3] , [0.299 , 0.587, 0.114])
@@ -75,3 +191,4 @@ def show_image(image):
     """
     plt.imshow(image, cmap='gray')
     plt.show()
+
